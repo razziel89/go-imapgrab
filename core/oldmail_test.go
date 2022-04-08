2e00d44be2651a631d347d9fc24bc767c67e81f3
Torsten Sachse <torsten.sachse@gmail.com>
2022-04-08 21:37:36 +0200

    Add tests for oldmail writeout

--- core/oldmail_test.go
+++ core/oldmail_test.go
@@ -21,7 +21,10 @@ import (
 	"bytes"
 	"os"
 	"path/filepath"
+	"strings"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -60,12 +63,13 @@ func TestOldmailRead(t *testing.T) {
 		{uidValidity: 123, uid: 15, timestamp: 898},
 		{uidValidity: 123, uid: 17, timestamp: 242},
 	}
-	oldmailContent := []byte(`123/21_747
-123/42_447
-123/11_321
-123/15_898
-123/17_242
-`)
+	oldmailContent := []byte(strings.Join([]string{
+		"123/21_747",
+		"123/42_447",
+		"123/11_321",
+		"123/15_898",
+		"123/17_242",
+	}, "\n"))
 	// Replace "_" by the null character to work around go strings ignoring the null byte.
 	oldmailContent = bytes.ReplaceAll(oldmailContent, []byte("_"), []byte{0})
 
@@ -79,4 +83,95 @@ func TestOldmailRead(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, expectedOldmails, oldmails)
+}
+
+func TestOldmailReadFileNotFound(t *testing.T) {
+	tmp := t.TempDir()
+	tmpFile := filepath.Join(tmp, "tmpfile")
+
+	_, err := readOldmail(tmpFile)
+
+	assert.Error(t, err)
+}
+
+func TestOldmailReadCannotRead(t *testing.T) {
+	tmp := t.TempDir()
+	tmpFile := filepath.Join(tmp, "tmpfile")
+
+	err := os.WriteFile(tmpFile, []byte{}, 0444)
+	assert.NoError(t, err)
+
+	err = os.Chmod(tmpFile, 0000)
+	assert.NoError(t, err)
+
+	_, err = readOldmail(tmpFile)
+
+	assert.Error(t, err)
+}
+
+func TestOldmailWriteout(t *testing.T) {
+	oldmails := []oldmail{
+		{uidValidity: 123, uid: 21, timestamp: 747},
+		{uidValidity: 123, uid: 42, timestamp: 447},
+		{uidValidity: 123, uid: 11, timestamp: 321},
+		{uidValidity: 123, uid: 15, timestamp: 898},
+		{uidValidity: 123, uid: 17, timestamp: 242},
+	}
+	expectedOldmailContent := []byte(strings.Join([]string{
+		"123/21_747",
+		"123/42_447",
+		"123/11_321",
+		"123/15_898",
+		"123/17_242",
+		"", // Files need to end with a newline character.
+	}, "\n"))
+	// Replace "_" by the null character to work around go strings ignoring the null byte.
+	expectedOldmailContent = bytes.ReplaceAll(expectedOldmailContent, []byte("_"), []byte{0})
+
+	oldmailChan := make(chan oldmail)
+	go func() {
+		for _, om := range oldmails {
+			oldmailChan <- om
+		}
+		close(oldmailChan)
+	}()
+
+	tmp := t.TempDir()
+	tmpFile := filepath.Join(tmp, "tmpfile")
+
+	var wg, stwg sync.WaitGroup
+	stwg.Add(1)
+
+	errCountPtr, err := streamingOldmailWriteout(oldmailChan, tmpFile, &wg, &stwg)
+	assert.NoError(t, err)
+
+	// Nothing written before stwg has been set to done.
+	time.Sleep(time.Millisecond * 100)
+	content, err := os.ReadFile(tmpFile) // nolint: gosec
+	assert.NoError(t, err)
+	assert.Empty(t, content)
+
+	// Stuff is written after stwg was set to done.
+	stwg.Done()
+	wg.Wait()
+	content, err = os.ReadFile(tmpFile) // nolint: gosec
+	assert.NoError(t, err)
+	assert.Equal(t, expectedOldmailContent, content)
+	assert.Zero(t, *errCountPtr)
+}
+
+func TestOldmailWriteoutFileNotFound(t *testing.T) {
+	tmp := t.TempDir()
+	tmpPath := filepath.Join(tmp, "tmpfile")
+
+	// Trigger an error below by having a directory where a file was expected.
+	err := os.Mkdir(tmpPath, 0555)
+	assert.NoError(t, err)
+
+	oldmailChan := make(chan oldmail)
+	close(oldmailChan)
+	var wg, stwg sync.WaitGroup
+
+	_, err = streamingOldmailWriteout(oldmailChan, tmpPath, &wg, &stwg)
+	assert.Error(t, err)
 }
