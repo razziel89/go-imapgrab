9ec60f20711e52d1cf435de986f30e6d27f08632
Torsten Sachse <torsten.sachse@gmail.com>
2022-06-17 16:42:33 +0200

    Outsource message delivery & tests into separate files

---
+++ core/deliver.go
@@ -0,0 +1,70 @@
+/* A re-implementation of the amazing imapgrap in plain Golang.
+Copyright (C) 2022  Torsten Sachse
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package core
+
+import "sync"
+
+type deliverOps interface {
+	deliverMessage(string, string) error
+	rfc822FromEmail(emailOps, int) (string, oldmail, error)
+}
+
+type deliverer struct{}
+
+func (d deliverer) deliverMessage(text string, maildirPath string) error {
+	return deliverMessage(text, maildirPath)
+}
+
+func (d deliverer) rfc822FromEmail(msg emailOps, uidvalidity int) (string, oldmail, error) {
+	return rfc822FromEmail(msg, uidvalidity)
+}
+
+func streamingDelivery(
+	ops deliverOps,
+	messageChan <-chan emailOps,
+	maildirPath string,
+	uidvalidity int,
+	wg, stwg *sync.WaitGroup,
+) (returnedChan <-chan oldmail, errCountPtr *int) {
+	var errCount int
+
+	deliveredChan := make(chan oldmail, messageDeliveryBuffer)
+
+	wg.Add(1)
+	go func() {
+		// Do not start before the entire pipeline has been set up.
+		stwg.Wait()
+		for msg := range messageChan {
+			// Deliver each email to the `tmp` directory and move them to the `new` directory.
+			text, oldmail, err := ops.rfc822FromEmail(msg, uidvalidity)
+			if err == nil {
+				err = ops.deliverMessage(text, maildirPath)
+			}
+			if err != nil {
+				logError(err.Error())
+				errCount++
+				continue
+			}
+			deliveredChan <- oldmail
+		}
+		wg.Done()
+		close(deliveredChan)
+	}()
+
+	return deliveredChan, &errCount
+}
