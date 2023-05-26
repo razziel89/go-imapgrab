ea42da85f807af7d89f4710968c174711ff9f20f
Torsten Long <torsten.s.long@gmail.com>
2023-05-26 22:57:17 +0200

    Update name in copyright header

--- core/ids_test.go
+++ core/ids_test.go
@@ -1,5 +1,5 @@
 /* A re-implementation of the amazing imapgrap in plain Golang.
-Copyright (C) 2022  Torsten Sachse
+Copyright (C) 2022  Torsten Long
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
